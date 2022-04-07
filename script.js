f80e03b0b5de32397f51d8835c2c8878d6a3cd29
Lucas Lafèré <laferelcas@gmail.com>
2022-04-07 15:26:32 -0300

    adicionada funcao basica de girar as cartas - só gira a img de dentro por enquanto, e adicionado prompt que pergunta qtd cartas e da loop caso seja impar

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".card-box");
+let qtd = prompt("Com quantas cartas deseja jogar?");
+while (qtd % 2 !== 0) {
+    qtd = prompt("Com quantas cartas deseja jogar?");
+}
+
+function qtdCard () {
+    for(let i = 0; i < qtd; i++){
+        //adicionar card ate i = qtd --- nao sei como fazer add card
+    }
+}
