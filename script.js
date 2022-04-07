99fc771c809fec31f4e557f3bd8655fbc29e047d
Lucas Lafèré <laferelcas@gmail.com>
2022-04-07 17:15:02 -0300

    cartas sendo distribuidas de acordo com o numero, array criada, imagens todas iguais

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-const container = document.querySelector(".card-box");
-let qtd = prompt("Com quantas cartas deseja jogar?");
-while (qtd % 2 !== 0) {
+const cardbox = document.querySelector(".card-box"); //localiza o card-box
+const card = document.querySelector(".card");
+const cardList = [];
+const imgLista = [
+'assets/bobrossparrot.gif',
+'assets/explodyparrot.gif',
+'assets/fiestaparrot.gif', 
+'assets/metalparrot.gif',
+'assets/reveritparrot.gif',
+'assets/tripletsparrot.gif',
+'assets/unicornparrot.gif'];
+
+let qtd = prompt("Com quantas cartas deseja jogar?"); //pergunta qtd cartas a jogar
+
+while (qtd % 2 !== 0 || qtd < 4 || qtd > 14) { //impede que seja impar, <4 e >14
     qtd = prompt("Com quantas cartas deseja jogar?");
+}
+if (qtd % 2 == 0 || qtd >= 4 || qtd <= 14){
+    qtdCard();
 }
 
 function qtdCard () {
     for(let i = 0; i < qtd; i++){
-        //adicionar card ate i = qtd --- nao sei como fazer add card
+        cardbox.innerHTML += `<div class="card">
+        <img src="assets/front.png" alt="front-parrot" class="front">
+        <img src="/assets/${imgLista[0,i]}" alt="" class="back">
+    </div>`;
+    cardList.push(i);
     }
 }
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
+cardList.sort(comparador); 
+
+
+
+
+
+// function addImage () { //teste que deu errado
+   
+
+//     card2.setAttribute("src", imgLista[6])
+// }
+
