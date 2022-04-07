391e995647febfb5278d400a84e40ff98ee2c677
Lucas Lafèré <laferelcas@gmail.com>
2022-04-07 18:23:40 -0300

    ta distribuindo em pares de imagens, falta embaralhar

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const cardbox = document.querySelector(".card-box"); //localiza o card-box
 const card = document.querySelector(".card");
 const cardList = [];
 const imgLista = [
-'assets/bobrossparrot.gif',
-'assets/explodyparrot.gif',
-'assets/fiestaparrot.gif', 
-'assets/metalparrot.gif',
-'assets/reveritparrot.gif',
-'assets/tripletsparrot.gif',
-'assets/unicornparrot.gif'];
+'bobrossparrot.gif',
+'explodyparrot.gif',
+'fiestaparrot.gif', 
+'metalparrot.gif',
+'revertitparrot.gif',
+'tripletsparrot.gif',
+'unicornparrot.gif'];
 
 let qtd = prompt("Com quantas cartas deseja jogar?"); //pergunta qtd cartas a jogar
 
@@ -23,26 +23,10 @@ function qtdCard () {
     for(let i = 0; i < qtd; i++){
         cardbox.innerHTML += `<div class="card">
         <img src="assets/front.png" alt="front-parrot" class="front">
-        <img src="/assets/${imgLista[0,i]}" alt="" class="back">
+        <img src="assets/${imgLista[i]}" alt="" class="back">
     </div>`;
     cardList.push(i);
     }
 }
 
-function comparador() { 
-	return Math.random() - 0.5; 
-}
-
-
-cardList.sort(comparador); 
-
-
-
-
-
-// function addImage () { //teste que deu errado
-   
-
-//     card2.setAttribute("src", imgLista[6])
-// }
 
