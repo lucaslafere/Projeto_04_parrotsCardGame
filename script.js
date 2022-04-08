54573258f3be16b9299cd24cca57a461cd03884a
Lucas Lafèré <laferelcas@gmail.com>
2022-04-08 13:00:30 -0300

    finalmente embaralha e em pares tudo certinho

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 const cardbox = document.querySelector(".card-box"); //localiza o card-box
-const card = document.querySelector(".card");
-const cardList = [];
+const cardList = []
 const imgLista = [
-'bobrossparrot.gif',
-'bobrossparrot.gif',
-'explodyparrot.gif',
-'explodyparrot.gif',
-'fiestaparrot.gif', 
-'fiestaparrot.gif', 
-'metalparrot.gif',
-'metalparrot.gif',
-'revertitparrot.gif',
-'revertitparrot.gif',
-'tripletsparrot.gif',
-'tripletsparrot.gif',
-'unicornparrot.gif',
-'unicornparrot.gif'];
+{ nome: "bob", img: 'bobrossparrot.gif'},
+{ nome: "bob", img: 'bobrossparrot.gif'},
+{ nome: "explody", img: 'explodyparrot.gif'},
+{ nome: "explody", img: 'explodyparrot.gif'},
+{ nome: "fiesta", img: 'fiestaparrot.gif'},
+{ nome: "fiesta", img: 'fiestaparrot.gif'},
+{ nome: "metal", img: 'metalparrot.gif'},
+{ nome: "metal", img: 'metalparrot.gif'},
+{ nome: "revertit", img: 'revertitparrot.gif'},
+{ nome: "revertit", img: 'revertitparrot.gif'},
+{ nome: "triplets", img: 'tripletsparrot.gif'},
+{ nome: "triplets", img: 'tripletsparrot.gif'},
+{ nome: "unicorn", img: 'unicornparrot.gif'},
+{ nome: "unicorn", img: 'unicornparrot.gif'},
+];
 
 //fazer um splice nessa lista resolve? divide depois embaralha, sla?
 
@@ -25,23 +25,29 @@ while (qtd % 2 !== 0 || qtd < 4 || qtd > 14) { //impede que seja impar, <4 e >14
     qtd = prompt("Com quantas cartas deseja jogar?");
 }
 if (qtd % 2 == 0 || qtd >= 4 || qtd <= 14){
-    qtdCard(); //executa distribuiçao das cartas
+    qtdCard(); //cria array com a qtd carta escolhidas
+    pickCard();
 }
 
 function qtdCard () {
+    for(let i = 0; i < (qtd); i++){
+    cardList.push(imgLista[i])
+    }
+    cardList.sort(comparador)
+}
 
+function pickCard (){
     for(let i = 0; i < (qtd); i++){
         cardbox.innerHTML += `<div class="card">
         <img src="assets/front.png" alt="front-parrot" class="front">
-        <img src="assets/${imgLista[i]}" alt="" class="back">
+        <img src="assets/${cardList[i].img}" alt="" class="back" id="${cardList[i].nome}">
     </div>`;
-    }
+}
 }
 
+
+cardList.sort(comparador);
 function comparador() { 
 	return Math.random() - 0.5; 
 }
 
-//vou ignorar tudo de cima, ja da pra distribuir "aleatoriamente"
-//mas ainda nao embaralha. Porem vem em pares, e sempre diferentes.
-//ajeito depois dessa parte de cima
