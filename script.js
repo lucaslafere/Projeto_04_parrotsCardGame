c39a2934edef48746da9217cdd3acc5b6e92d281
Lucas Lafèré <laferelcas@gmail.com>
2022-04-08 12:27:05 -0300

    tentei tentei tentei e nao deu em nada, nao sei as mudanças mas vou salvar pra parar por agora

--- script.js
+++ script.js
@@ -3,13 +3,20 @@ const card = document.querySelector(".card");
 const cardList = [];
 const imgLista = [
 'bobrossparrot.gif',
+'bobrossparrot.gif',
+'explodyparrot.gif',
 'explodyparrot.gif',
 'fiestaparrot.gif', 
+'fiestaparrot.gif', 
+'metalparrot.gif',
 'metalparrot.gif',
 'revertitparrot.gif',
+'revertitparrot.gif',
 'tripletsparrot.gif',
+'tripletsparrot.gif',
+'unicornparrot.gif',
 'unicornparrot.gif'];
-imgLista.sort(comparador);
+
 //fazer um splice nessa lista resolve? divide depois embaralha, sla?
 
 let qtd = prompt("Com quantas cartas deseja jogar?"); //pergunta qtd cartas a jogar
@@ -18,26 +25,23 @@ while (qtd % 2 !== 0 || qtd < 4 || qtd > 14) { //impede que seja impar, <4 e >14
     qtd = prompt("Com quantas cartas deseja jogar?");
 }
 if (qtd % 2 == 0 || qtd >= 4 || qtd <= 14){
-    qtdCard();
+    qtdCard(); //executa distribuiçao das cartas
 }
 
 function qtdCard () {
-    for(let i = 0; i < (qtd/2); i++){
+
+    for(let i = 0; i < (qtd); i++){
         cardbox.innerHTML += `<div class="card">
         <img src="assets/front.png" alt="front-parrot" class="front">
         <img src="assets/${imgLista[i]}" alt="" class="back">
-    </div>
-    <div class="card">
-        <img src="assets/front.png" alt="front-parrot" class="front">
-        <img src="assets/${imgLista[i]}" alt="" class="back">
     </div>`;
-    cardList.push(i);
     }
 }
 
-// Após esta linha, a minhaArray estará embaralhada
-
-// Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
 	return Math.random() - 0.5; 
 }
+
+//vou ignorar tudo de cima, ja da pra distribuir "aleatoriamente"
+//mas ainda nao embaralha. Porem vem em pares, e sempre diferentes.
+//ajeito depois dessa parte de cima
